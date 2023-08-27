be2eeaa2127c83fb4fe298f48545183d5e078f22
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-08-27 12:24:19 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,28 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 import Cell from "./Cell";
+import Grid from "./Grid";
 
 function App() {
   // const [count, setCount] = useState(0);
+
+  function create2DArray(width, height) {
+    const array2D = [];
+
+    for (let i = 0; i < height; i++) {
+      const row = [];
+      for (let j = 0; j < width; j++) {
+        row.push(<Cell i={i} j={j} />); // You can initialize the values as needed
+      }
+      array2D.push(row);
+    }
+
+    return array2D;
+  }
+
+  let width = 10;
+  let height = 10;
+  const grid = create2DArray(width, height);
 
   return (
     <>
@@ -30,9 +49,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <Cell />
-      <Cell />
-      <Cell />
+      {grid}
+      {/* <Grid height={10} width={10} /> */}
+      <div className="w-1/2 mx-auto mt-10">
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+      </div>
     </>
   );
 }
