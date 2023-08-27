d12d0b4dae0699f31c33f28ee9e7772e4daee7aa
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-08-27 13:08:20 -0400

    Update Grid.jsx

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,9 +1,40 @@
-// import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-// function Cell() {
-//   const [status, setStatus] = useState(0);
+import Cell from "./Cell";
 
-//   return <span className="border border-slate-500">{status}</span>;
-// }
+function Grid({ width, height }) {
+  function create2DArray(width, height) {
+    const array2D = [];
 
-// export default Cell;
+    for (let i = 0; i < height; i++) {
+      const row = [];
+      for (let j = 0; j < width; j++) {
+        row.push(<Cell key={`${i}-${j}`} i={i} j={j} />);
+      }
+      array2D.push(row);
+    }
+
+    return array2D;
+  }
+
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    const newArray = create2DArray(width, height);
+    setValues(newArray);
+  }, [width, height]);
+
+  return (
+    <div className="grid">
+      {values.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          {row.map((cell, cellIndex) => {
+            return cell;
+          })}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Grid;
