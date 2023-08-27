4c0c8f5d601aa35e531c63844c433343e45c75f3
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-08-27 11:18:34 -0400

    Add onClick function

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -3,7 +3,18 @@ import { useState } from "react";
 function Cell() {
   const [status, setStatus] = useState(0);
 
-  return <span className="border border-slate-500">{status}</span>;
+  function onClick() {
+    setStatus((prevStatus) => prevStatus ^ 1);
+  }
+
+  return (
+    <span
+      className="border border-slate-500 p-4 select-none cursor-pointer hover:bg-blue-100"
+      onClick={onClick}
+    >
+      {status}
+    </span>
+  );
 }
 
 export default Cell;
---
+++ src/Grid.jsx
@@ -0,0 +1,9 @@
+// import { useState } from "react";
+
+// function Cell() {
+//   const [status, setStatus] = useState(0);
+
+//   return <span className="border border-slate-500">{status}</span>;
+// }
+
+// export default Cell;
