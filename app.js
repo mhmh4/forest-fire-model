6e94b6124994b78e340700f7fa01117e685e7c4d
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-04 15:03:22 -0400

    Draw some cells

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("test");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const CELL_LENGTH = 25;
+
+const m = canvas.width / CELL_LENGTH;
+const n = canvas.height / CELL_LENGTH;
+
+let grid = [...Array(m)].map(() => Array(n));
+
+console.log(grid);
+
+function drawCells() {
+  for (let j = 0; j < canvas.height; j += CELL_LENGTH) {
+    for (let i = 0; i < canvas.width; i += CELL_LENGTH) {
+      ctx.rect(i, j, CELL_LENGTH, CELL_LENGTH);
+      ctx.stroke();
+    }
+  }
+}
+
+drawCells(grid);
